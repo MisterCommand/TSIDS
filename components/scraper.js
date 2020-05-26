7b4a2cbcc042fcb61ab77caebb0ec4a1b07f0d9d
MisterCommand <anthonyhou04@gmail.com>
2020-05-26 21:36:28 +0800

    🌐News selection update

--- components/scraper.js
+++ components/scraper.js
@@ -8,10 +8,11 @@ function news() {
         request("http://news.tvb.com/list/instant", (error, response, html) => {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html) // Make it acts like JQuery
-            
+                newsList = [];
                 $("body div.list_container.clearfix div.container.c1 div div:nth-child(1) div.dailyNews div").each((index, data) => { // For each row column
-                    if (!$(data).find("a").text().includes("《") && !$(data).find("a").text().includes("[") && !$(data).find("a").text().includes("無綫") && $(data).find("a").text() != "") { // If text doesn't include 《 or [ or 無綫 and not empty
-                    newsList.push($(data).find("a").text().replace("\n\t\t\n\t\t\t\t\t",''))
+                    let text = $(data).find("a").text();
+                    if (!text.includes("《") && !text.includes("[不斷更新]") && !text.includes("[現場]") && !text.includes("無綫") && text != "") { // If text doesn't include 《 or [ or 無綫 or [不斷更新] or [現場]and not empty
+                    newsList.push(text.replace("\n\t\t\n\t\t\t\t\t",''))
                     }
                 })
                 resolve(newsList)
